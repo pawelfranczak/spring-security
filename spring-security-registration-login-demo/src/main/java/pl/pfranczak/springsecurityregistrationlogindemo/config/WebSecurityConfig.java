1120124bedee250c8cddab59d9fa1579e5c66c05
Pawel Franczak <gzem89@gmail.com>
2020-10-08 09:41:32 +0200

    clean

--- spring-security-registration-login-demo/src/main/java/pl/pfranczak/springsecurityregistrationlogindemo/config/WebSecurityConfig.java
+++ spring-security-registration-login-demo/src/main/java/pl/pfranczak/springsecurityregistrationlogindemo/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package pl.pfranczak.springsecurityregistrationlogindemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,7 +30,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginPage("/sign-in")
 			.permitAll();
 
-//		http.authorizeRequests().antMatchers("/h2-console/**").permitAll().anyRequest().authenticated();
 		http.csrf().disable();
 		http.headers().frameOptions().sameOrigin();
 
